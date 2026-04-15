6ed9c006d9012ceb9643abc6489586cd0354aed6
umidjon11111 <umidjongafforov844@gmail.com>
2026-04-15 15:36:29 +0500

    :fix payme v7

--- src/services/paymeService.js
+++ src/services/paymeService.js
@@ -35,11 +35,6 @@ export const checkPerformTransaction = async ({ id, params }) => {
     return PaymeError.wrongAmount(id);
   }
 
-  // Agar obuna allaqachon pending bo'lsa, boshqa tranzaksiya yaratish mumkin emas
-  if (sub.paymeState === STATE.Pending && sub.paymeTransactionId) {
-    return PaymeError.cantDoOperation(id);
-  }
-
   // Agar obuna allaqachon to'langan bo'lsa
   if (sub.paymeState === STATE.Paid) {
     return PaymeError.alreadyDone(id);
@@ -62,11 +57,6 @@ export const createTransaction = async ({ id, params }) => {
   );
 
   if (!sub) return PaymeError.orderNotFound(id);
-
-  // Agar obuna allaqachon pending bo'lsa, xato qaytarish
-  if (sub.paymeState === STATE.Pending && sub.paymeTransactionId) {
-    return PaymeError.pending(id);
-  }
 
   // Agar obuna allaqachon to'langan bo'lsa
   if (sub.paymeState === STATE.Paid) {
